5048662ef312ee19a72347a9b1c5a79afa9cb8d5
whitebelyash <whbexiumwork@gmail.com>
2026-07-24 14:03:17 +0400

    Correct shader stages in push constants command

--- src/main/java/git/artdeell/compat/sodium/VKDrawContextMixin.java
+++ src/main/java/git/artdeell/compat/sodium/VKDrawContextMixin.java
@@ -9,6 +9,7 @@ import net.caffeinemc.mods.sodium.client.gpu.device.context.VKDrawContext;
 import net.caffeinemc.mods.sodium.client.render.chunk.region.RenderRegion;
 import net.caffeinemc.mods.sodium.client.render.viewport.CameraTransform;
 import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.VK10;
 import org.lwjgl.vulkan.VK11;
 import org.lwjgl.vulkan.VkCommandBuffer;
 import org.objectweb.asm.Opcodes;
@@ -42,7 +43,7 @@ public class VKDrawContextMixin {
 
     @Inject(method = "updateData", at = @At(value = "INVOKE", target = "Lorg/lwjgl/vulkan/VK13;nvkCmdPushConstants(Lorg/lwjgl/vulkan/VkCommandBuffer;JIIIJ)V"), cancellable = true)
     public void injectPushConstant(RenderRegion region, CameraTransform camera, CallbackInfo ci, @Local(name = "memory") long memory, @Local(name = "stack") MemoryStack stack){
-        VK11.nvkCmdPushConstants(this.cmdBuf, this.layout, VK11.VK_SHADER_STAGE_ALL, 0, DrawContext.PUSH_CONSTANT_RANGE, memory);
+        VK11.nvkCmdPushConstants(this.cmdBuf, this.layout, VK10.VK_SHADER_STAGE_VERTEX_BIT | VK10.VK_SHADER_STAGE_FRAGMENT_BIT, 0, DrawContext.PUSH_CONSTANT_RANGE, memory);
         stack.close();
         ci.cancel();
     }
